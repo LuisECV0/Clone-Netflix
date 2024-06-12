3c0b7225607390b9dcbb00293258d396bfdd83f3
Luiscv1719 <luiscv1719@gmail.com>
2024-06-11 22:41:13 -0500

    commit dia 11

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Banner.css';
 
 function Banner() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email) {
+      alert(`El correo electrónico ingresado es: ${email}`);
+    }
+  };
+
   return (
     <div className="banner">
-      <h1>Películas ilimitadas, programas de televisión y más</h1>
-      <p>Mira en cualquier lugar. Cancela en cualquier momento.</p>
-      <p>Listo para mirar? Introduzca su correo electrónico para crear o reiniciar su membresía.</p>
-        <div>
-          <input type='text' placeholder='asas'>
-          </input>
-          <a className='button_1'>Empezar</a>
-        </div>
+      <h1>Películas y series ilimitadas y mucho más</h1>
+      <p>Disfruta donde quieras. Cancela cuando quieras.</p>
+      <p>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix.</p>
+      <form onSubmit={handleSubmit}>
+        <input  
+          className='input-email'
+          type='email' 
+          placeholder='Email' 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit" className='button_1'>
+         <span className='txt'>Comenzar</span> 
+          <i className="fa-solid fa-chevron-right"></i>
+          </button>
+      </form>
     </div>
   );
 }
